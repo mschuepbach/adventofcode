69f259f2b86589ca8d76defb7bb048918cf5b217
mschuepbach <mschuepbach@pm.me>
2023-12-01 20:08:34 +0100

    Add day 1 part 2

--- 2023/01/Program.cs
+++ 2023/01/Program.cs
@@ -16,8 +16,19 @@ void Part1()
 
 void Part2()
 {
+    string[] numbers = [ "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ..Enumerable.Range(1, 9).Select(i => i.ToString())];
 
-    Console.WriteLine($"Part 2: ");
+    var result = input
+        .Sum(line =>
+        {
+            var numbersInLine = numbers.Where(n => line.Contains(n));
+            var first = Array.IndexOf(numbers, numbersInLine.MinBy(n => line.IndexOf(n))) % 9 + 1;
+            var last = Array.IndexOf(numbers, numbersInLine.MaxBy(n => line.LastIndexOf(n))) % 9 + 1;
+            return int.Parse($"{first}{last}");
+        });
+
+    Console.WriteLine($"Part 2: {result}");
 }
 
 Part1();
