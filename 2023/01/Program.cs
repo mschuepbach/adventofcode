76945f18713b2fd54734c1ac9a69914cf761db94
mschuepbach <mschuepbach@pm.me>
2023-12-01 07:27:20 +0100

    Add day 1 part 1

--- 2023/01/Program.cs
+++ 2023/01/Program.cs
@@ -1,10 +1,17 @@
 ﻿
-var input = await File.ReadAllTextAsync("input.txt");
+var input = await File.ReadAllLinesAsync("input.txt");
 
 void Part1()
 {
+    var result = input
+        .Sum(line =>
+        {
+            var first = line.First(character => char.IsDigit(character));
+            var last = line.Last(character => char.IsDigit(character));
+            return int.Parse($"{first}{last}");
+        });
 
-    Console.WriteLine($"Part 1: ");
+    Console.WriteLine($"Part 1: {result}");
 }
 
 void Part2()
