47f10486dbdf3387158336af7186af615473cc50
mschuepbach <mschuepbach@pm.me>
2023-12-02 10:54:18 +0100

    Add day 2 part 2

--- 2023/02/Program.cs
+++ 2023/02/Program.cs
@@ -43,8 +43,39 @@ void Part1()
 
 void Part2()
 {
+    var result = 0;
 
-    Console.WriteLine($"Part 2: ");
+    foreach (var line in input)
+    {
+        var x = line.Split(':');
+        var gameId = int.Parse(x[0].Replace("Game ", null));
+        var games = x[1].Split(";");
+        var maxRed = 0;
+        var maxGreen = 0;
+        var maxBlue = 0;
+
+        foreach (var game in games)
+        {
+            var sets = game.Split(",");
+            foreach (var set in sets)
+            {
+                var y = set.Trim().Split(' ');
+                var number = int.Parse(y[0]);
+                var color = y[1];
+
+                if (color == "red" && number > maxRed)
+                    maxRed = number;
+                else if (color == "green" && number > maxGreen)
+                    maxGreen = number;
+                else if (color == "blue" && number > maxBlue)
+                    maxBlue = number;
+            }
+        }
+
+        result += maxRed * maxGreen * maxBlue;
+    }
+
+    Console.WriteLine($"Part 2: {result}");
 }
 
 Part1();
